39ea3507ffdf6699bb98efcb8b4ef842deaac585
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2023-05-30 21:09:15 +0600

    16th solution

--- src/main/java/jdbc/CustomDataSource.java
+++ src/main/java/jdbc/CustomDataSource.java
@@ -5,9 +5,7 @@ import javax.sql.DataSource;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
@@ -37,34 +35,35 @@ public class CustomDataSource implements DataSource {
     }
 
     public static CustomDataSource getInstance() {
-
         if (instance == null) {
             Properties props = new Properties();
             ClassLoader loader = Thread.currentThread().getContextClassLoader();
             InputStream stream = loader.getResourceAsStream("app.properties");
+
             try {
                 props.load(stream);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            instance = new CustomDataSource(
-                    props.getProperty("postgres.driver"),
-                    props.getProperty("postgres.url"),
-                    props.getProperty("postgres.password"),
-                    props.getProperty("postgres.name"));
+
+            String driver = props.getProperty("postgres.driver");
+            String url = props.getProperty("postgres.url");
+            String password = props.getProperty("postgres.password");
+            String name = props.getProperty("postgres.name");
+
+            instance = new CustomDataSource(driver, url, password, name);
         }
         return instance;
     }
 
     @Override
     public Connection getConnection() throws SQLException {
-        CustomConnector connector = new CustomConnector();
-        return connector.getConnection(url, name, password);
+        return new CustomConnector().getConnection(url, name, password);
     }
 
     @Override
-    public Connection getConnection(String username, String password) throws SQLException {
-        throw new UnsupportedOperationException();
+    public Connection getConnection(String username, String password) {
+        return new CustomConnector().getConnection(url, username, password);
     }
 
     @Override
@@ -99,6 +98,6 @@ public class CustomDataSource implements DataSource {
 
     @Override
     public boolean isWrapperFor(Class<?> iface) throws SQLException {
-        return false;
+        throw new UnsupportedOperationException();
     }
 }
